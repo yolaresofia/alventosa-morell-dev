570bc39d3fdcfbf72b857841bd61db4bf035631a
yolaresofia <yolaresofia@gmail.com>
2025-05-06 22:44:48 +0200

    update

--- nextjs-app/app/projects/components/ProjectsIndex.tsx
+++ nextjs-app/app/projects/components/ProjectsIndex.tsx
@@ -60,18 +60,16 @@ export default function ProjectsIndex({ projects }: ProjectsIndexProps) {
               </Link>
 
               {isHovered && project.thumbnail && (
-                <div className="grid grid-cols-5 py-2">
-                  <div className="col-span-1">
-                    <div className="relative w-full aspect-[4/3]">
-                      <Image
-                        src={urlForImage(project.thumbnail)?.url() || ""}
-                        alt={project.title}
-                        fill
-                        className="object-cover"
-                      />
-                    </div>
+                <div className="py-2">
+                  <div className="relative h-64 w-auto">
+                    <Image
+                      src={urlForImage(project.thumbnail)?.url() || ""}
+                      alt={project.title}
+                      fill
+                      className="object-contain"
+                      style={{ objectPosition: "left" }}
+                    />
                   </div>
-                  <div className="col-span-4"></div>
                 </div>
               )}
             </div>
